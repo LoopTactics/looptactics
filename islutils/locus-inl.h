1301c7982703ed3829b7352c6d6cf1f5e4dad58d
Oleksandr Zinenko <git@ozinenko.com>
2018-06-14 23:29:12 +0200

    introduce map-constructing overloaded operators for aff and pw_aff
    As discussed in th PollyLabs issue tracker, put overloaded operators into a
    different namespace to avoid ambiguous overloads with set-constructing
    operators.  Note that as of isl-0.18-1063-g7c031247, isl only provides eq, lt
    and gt map-construction functions and on pw_aff, without exporting them to C++.
    Call C functions with (un)wrapping instead.  Other operators are expressed in
    terms of these functions locally to islutils.  Plain affs are first lifted to
    pw_aff and then submitted to the pw_aff versions of the respective operators.

--- islutils/locus-inl.h
+++ islutils/locus-inl.h
@@ -49,3 +49,64 @@ inline isl::set operator>(isl::pw_aff lhs, isl::pw_aff rhs) {
   return lhs.gt_set(rhs);
 }
 } // namespace set_maker
+
+namespace map_maker {
+
+/* isl::aff operators ********************************************************/
+// map-producing functions are not available for affs, raise to pw_aff first
+inline isl::map operator==(isl::aff lhs, isl::aff rhs) {
+  return isl::pw_aff(lhs) == isl::pw_aff(rhs);
+}
+
+inline isl::map operator!=(isl::aff lhs, isl::aff rhs) {
+  return isl::pw_aff(lhs) != isl::pw_aff(rhs);
+}
+
+inline isl::map operator>=(isl::aff lhs, isl::aff rhs) {
+  return isl::pw_aff(lhs) >= isl::pw_aff(rhs);
+}
+
+inline isl::map operator>(isl::aff lhs, isl::aff rhs) {
+  return isl::pw_aff(lhs) > isl::pw_aff(rhs);
+}
+
+inline isl::map operator<=(isl::aff lhs, isl::aff rhs) {
+  return isl::pw_aff(lhs) <= isl::pw_aff(rhs);
+}
+
+inline isl::map operator<(isl::aff lhs, isl::aff rhs) {
+  return isl::pw_aff(lhs) < isl::pw_aff(rhs);
+}
+
+/* isl::pw_aff operators *****************************************************/
+inline isl::map operator==(isl::pw_aff lhs, isl::pw_aff rhs) {
+  // not exported
+  return isl::manage(isl_pw_aff_eq_map(lhs.release(), rhs.release()));
+}
+
+inline isl::map operator!=(isl::pw_aff lhs, isl::pw_aff rhs) {
+  // not available directly
+  return (lhs > rhs).unite(lhs < rhs);
+}
+
+inline isl::map operator<=(isl::pw_aff lhs, isl::pw_aff rhs) {
+  // not available directly
+  return (lhs == rhs).unite(lhs < rhs);
+}
+
+inline isl::map operator<(isl::pw_aff lhs, isl::pw_aff rhs) {
+  // not exported
+  return isl::manage(isl_pw_aff_lt_map(lhs.release(), rhs.release()));
+}
+
+inline isl::map operator>=(isl::pw_aff lhs, isl::pw_aff rhs) {
+  // not available directly
+  return (lhs == rhs).unite(lhs > rhs);
+}
+
+inline isl::map operator>(isl::pw_aff lhs, isl::pw_aff rhs) {
+  // not exported
+  return isl::manage(isl_pw_aff_gt_map(lhs.release(), rhs.release()));
+}
+
+} // namespace map_maker
--- islutils/locus.h
+++ islutils/locus.h
@@ -29,4 +29,33 @@ isl::set operator<(isl::pw_aff lhs, isl::pw_aff rhs);
 /// \}
 } // namespace set_maker
 
+namespace map_maker {
+
+/// \defgroup AffMapMaker Constructing isl::map from isl::aff
+/// \{
+/// Overloaded operators building a map in the (common) space of the given
+/// affine functions.  The map is constrained by the selected binary operation
+/// between the given affine functions.
+isl::map operator==(isl::aff lhs, isl::aff rhs);
+isl::map operator!=(isl::aff lhs, isl::aff rhs);
+isl::map operator>=(isl::aff lhs, isl::aff rhs);
+isl::map operator>(isl::aff lhs, isl::aff rhs);
+isl::map operator<=(isl::aff lhs, isl::aff rhs);
+isl::map operator<(isl::aff lhs, isl::aff rhs);
+/// \}
+
+/// \defgroup PwAffMapMaker Constructing isl::map from isl::pw_aff
+/// \{
+/// Overloaded operators building a map in the (common) space of the given
+/// piecewise affine functions.  The map is constrained by the selected binary
+/// operation between the given piecewise affine functions.
+isl::map operator==(isl::pw_aff lhs, isl::pw_aff rhs);
+isl::map operator!=(isl::pw_aff lhs, isl::pw_aff rhs);
+isl::map operator>=(isl::pw_aff lhs, isl::pw_aff rhs);
+isl::map operator>(isl::pw_aff lhs, isl::pw_aff rhs);
+isl::map operator<=(isl::pw_aff lhs, isl::pw_aff rhs);
+isl::map operator<(isl::pw_aff lhs, isl::pw_aff rhs);
+/// \}
+} // namespace map_maker
+
 #include "locus-inl.h"
