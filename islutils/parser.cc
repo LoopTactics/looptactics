a774279b9599bf4768fe91801676ea7dc8417616
Tobias Grosser <tobias@grosser.es>
2018-08-09 17:18:42 +0200

    use tagged accesses

--- islutils/parser.cc
+++ islutils/parser.cc
@@ -9,9 +9,9 @@ Scop Parser::getScop() {
   isl_ctx *ctx = isl_ctx_alloc_with_pet_options();
   pet_scop *scop = pet_scop_extract_from_C_source(ctx, Filename.c_str(), NULL);
   S.schedule = isl::manage(pet_scop_get_schedule(scop));
-  S.reads = isl::manage(pet_scop_get_may_reads(scop));
-  S.mayWrites = isl::manage(pet_scop_get_may_writes(scop));
-  S.mustWrites = isl::manage(pet_scop_get_must_writes(scop));
+  S.reads = isl::manage(pet_scop_get_tagged_may_reads(scop));
+  S.mayWrites = isl::manage(pet_scop_get_tagged_may_writes(scop));
+  S.mustWrites = isl::manage(pet_scop_get_tagged_must_writes(scop));
 
   return S;
 }
