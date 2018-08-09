dbfd27b3d64d3d3792bb33f7638760df1073c8d7
Tobias Grosser <tobias@grosser.es>
2018-08-09 12:08:39 +0200

    Convert set maker to gtest

--- test/test_set_maker.cc
+++ test/test_set_maker.cc
@@ -1,8 +1,19 @@
 #include <islutils/locus.h>
 
-int main() {
-  isl::aff a1;
-  isl::aff a2;
-  using map_maker::operator==;
-  auto x = (a1 == a2);
+#include "gtest/gtest.h"
+
+TEST(TestMatcher, Equality) { 
+  isl::ctx ctx = isl_ctx_alloc();
+  { 
+  	isl::aff a1(ctx, "{[i] -> [(1)]})");
+  	isl::aff a2(ctx, "{[i] -> [(1)]})");
+  	isl::map m(ctx, "{[j] -> [i]})");
+  	using map_maker::operator==;
+  	EXPECT_TRUE((a1 == a2).is_equal(m));
+  }
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
