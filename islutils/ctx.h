4a4a1f99ff715635670f4577fd0c4ec0ddee5255
Oleksandr Zinenko <git@ozinenko.com>
2018-08-29 13:36:17 +0200

    Introduce ScopedCtx
    Simple wrapper for the isl::ctx object that calls isl_ctx_free on destruction.

---
+++ islutils/ctx.h
@@ -0,0 +1,34 @@
+#ifndef ISLUTILS_CTX_H
+#define ISLUTILS_CTX_H
+
+#include <isl/ctx.h>
+
+namespace util {
+
+/// Simple wrapper around isl::ctx that allocates a new context during
+/// construction and frees it during destruction, e.g. when a stack-allocated
+/// instance of ScopedCtx goes out of scope.
+/// Implicitly convertible to both isl_ctx* and isl::ctx for convenience.
+/// Intentionally not copy-constructible or copy-assignable as it would have
+/// required reference counting.  Move-constructible to enable ownership
+/// transfer.
+class ScopedCtx {
+public:
+  ScopedCtx() : ctx(isl_ctx_alloc()) {}
+  ScopedCtx(const ScopedCtx &) = delete;
+  ScopedCtx(ScopedCtx &&) = default;
+  ~ScopedCtx() { isl_ctx_free(ctx.release()); }
+
+  ScopedCtx &operator=(const ScopedCtx &) = delete;
+  ScopedCtx &operator=(ScopedCtx &&) = default;
+
+  operator isl::ctx() { return ctx; }
+  operator isl_ctx *() { return ctx.get(); }
+
+private:
+  isl::ctx ctx;
+};
+
+} // namespace util
+
+#endif
