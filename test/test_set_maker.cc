55e6d7ae2be59573ef74ebe7bb8125a19bd3625b
Oleksandr Zinenko <git@ozinenko.com>
2018-09-11 16:05:40 +0200

    test_set_maker.cc: fix code style
    The test was using a wrong (copy-pasted) name and was not following the style
    guide.  Change the name to SetMaker and run clang-format.

--- test/test_set_maker.cc
+++ test/test_set_maker.cc
@@ -2,14 +2,14 @@
 
 #include "gtest/gtest.h"
 
-TEST(TestMatcher, Equality) { 
+TEST(SetMaker, Equality) {
   isl::ctx ctx = isl_ctx_alloc();
-  { 
-  	isl::aff a1(ctx, "{[i] -> [(1)]})");
-  	isl::aff a2(ctx, "{[i] -> [(1)]})");
-  	isl::map m(ctx, "{[j] -> [i]})");
-  	using map_maker::operator==;
-  	EXPECT_TRUE((a1 == a2).is_equal(m));
+  {
+    isl::aff a1(ctx, "{[i] -> [(1)]})");
+    isl::aff a2(ctx, "{[i] -> [(1)]})");
+    isl::map m(ctx, "{[j] -> [i]})");
+    using map_maker::operator==;
+    EXPECT_TRUE((a1 == a2).is_equal(m));
   }
 }
 
