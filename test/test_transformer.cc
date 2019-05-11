acc24476ca8e9128e27b77d99d85bee05a0c38cc
lorenzo chelini <l.chelini@icloud.com>
2019-05-11 15:02:34 +0200

    test overloading of allOf

--- test/test_transformer.cc
+++ test/test_transformer.cc
@@ -10,6 +10,7 @@
 #include <islutils/pet_wrapper.h>
 #include <islutils/aff_op.h>
 #include <islutils/cout_overloading.h>
+#include <islutils/access.h>
 
 namespace conversion {
 
@@ -1976,6 +1977,86 @@ static isl::multi_union_pw_aff swapDims(isl::multi_union_pw_aff ps,
   ps = ps.set_union_pw_aff(secondDim, scheduleFirstDim);
   ps = ps.set_union_pw_aff(firstDim, scheduleSecondDim);
   return ps;
+}
+
+TEST(Transformer, synthesis) {
+
+  auto ctx = ScopedCtx(pet::allocCtx());
+  auto petScop = pet::Scop::parseFile(ctx, "inputs/1mmWithoutInitStmt.c");
+  auto scop = petScop.getScop();
+
+  auto dependences = computeAllDependences(scop);
+  scop.schedule =
+    mergeIfTilable(scop.schedule.get_root(), dependences).get_schedule();
+
+  isl::union_map reads = scop.reads.curry();
+  isl::union_map writes = scop.mustWrites.curry();
+
+  using namespace matchers;
+
+  typedef Placeholder<SingleInputDim,UnfixedOutDimPattern<SimpleAff>> Placeholder;
+  struct PlaceholderSet {
+    Placeholder p;
+    std::string id;
+  };
+  struct ArrayPlaceholderSet {
+    ArrayPlaceholder ap;
+    std::string id;
+  };
+
+  std::vector<PlaceholderSet> vectorPlaceholderSet = {};
+  std::vector<ArrayPlaceholderSet> vectorArrayPlaceholderSet = {};
+  
+  std::string id = "_i";
+  for (size_t i = 0; i < 3; i++) {
+    PlaceholderSet tmp = {placeholder(ctx), id+std::to_string(i)}; 
+    vectorPlaceholderSet.push_back(tmp);
+  }
+
+  id = "_C";
+  for (size_t i = 0; i < 3; i++) {
+    ArrayPlaceholderSet tmp = {arrayPlaceholder(), id+std::to_string(i)};
+    vectorArrayPlaceholderSet.push_back(tmp);
+  }
+
+  //for (size_t i = 0; i < 3; i++) {
+  //  std::cout << vectorPlaceholderSet[i].id << std::endl;
+  //  std::cout << vectorArrayPlaceholderSet[i].id << std::endl;
+  //}
+
+  typedef ArrayPlaceholderList<SingleInputDim, FixedOutDimPattern<SimpleAff>> Access;
+  std::vector<Access> accessList = {};
+
+  accessList.push_back(access(vectorArrayPlaceholderSet[0].ap,  // _C0 
+                              vectorPlaceholderSet[0].p,        // _i0 -> i
+                              vectorPlaceholderSet[1].p));      // _i1 -> j
+  accessList.push_back(access(vectorArrayPlaceholderSet[1].ap,
+                              vectorPlaceholderSet[0].p,
+                              vectorPlaceholderSet[2].p));
+  accessList.push_back(access(vectorArrayPlaceholderSet[2].ap,
+                              vectorPlaceholderSet[2].p,
+                              vectorPlaceholderSet[1].p));
+  auto psRead = allOf(accessList);
+  auto readMatches = match(reads, psRead);
+
+  auto _i = placeholder(ctx);
+  auto _j = placeholder(ctx);
+  auto _k = placeholder(ctx);
+  auto _C = arrayPlaceholder();
+  auto _B = arrayPlaceholder();
+  auto _A = arrayPlaceholder();
+
+  auto psReadOrig = 
+    allOf(access(_A, _i, _j), access(_B, _i, _k), access(_C, _k, _j));
+  auto readMatchesOrig = match(reads, psReadOrig);
+  
+  EXPECT_EQ(readMatches[0][vectorPlaceholderSet[0].p].payload().inputDimPos_,
+            readMatchesOrig[0][_i].payload().inputDimPos_);
+  EXPECT_EQ(readMatches[0][vectorPlaceholderSet[1].p].payload().inputDimPos_,
+            readMatchesOrig[0][_j].payload().inputDimPos_);
+  EXPECT_EQ(readMatches[0][vectorPlaceholderSet[2].p].payload().inputDimPos_,
+            readMatchesOrig[0][_k].payload().inputDimPos_);
+  
 }
 
 TEST(Transformer, MatchMatmul) {
