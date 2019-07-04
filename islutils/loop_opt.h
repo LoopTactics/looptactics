aca234ba5d75b1b2cd85882be029b48b237c1383
lorenzo chelini <l.chelini@icloud.com>
2019-07-04 14:37:31 +0200

    this class will perform all the code transformation. It is used by the tuner and tactics

---
+++ islutils/loop_opt.h
@@ -0,0 +1,33 @@
+#ifndef ISLUTILS_LOOP_OPT_H
+#define ISLUTILS_LOOP_OPT_H
+
+#include <string>       // std::string
+#include <fstream>    // std::ifstream
+#include <islutils/pet_wrapper.h>
+#include <islutils/ctx.h>
+#include <islutils/matchers.h>
+#include <islutils/builders.h>
+#include <islutils/error.h>
+
+namespace LoopTactics {
+
+  class LoopOptimizer {
+
+    public:
+      isl::schedule tile(const std::string loop_id, const int tile_size, isl::schedule schedule);
+      std::string code_gen(isl::schedule_node);
+      LoopOptimizer() = delete;
+      LoopOptimizer(const std::string path_to_file);
+
+    private:
+      static bool check_file_path(const std::string path_to_file);
+      pet::Scop scop_;
+  };
+
+
+} // end namespace loop tactics
+
+
+
+
+#endif 
