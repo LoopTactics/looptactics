b08668d461f83a763ac81c6dbee9fa6cc9f42118
lorenzo chelini <l.chelini@icloud.com>
2019-09-09 17:27:03 +0200

    clean-up main

--- main.cc
+++ main.cc
@@ -1,31 +1,3 @@
-/*
-#include <iostream>
-#include "islutils/tactics.h"
-
-int main() {
-
-  using namespace LoopTactics;
-  using namespace Error;
-  try {
-    std::string tactics_id = "tactics_gemm_no_init";
-    std::string pattern = "C(i, j)+= A(i, k)*B(k,j)";
-    std::string path_to_file = "./test/inputs/gemm.c";
-
-    Tactics t = Tactics(tactics_id, pattern, path_to_file);
-    t.match();
-    t.tile("i", {32});
-    t.tile("k", {32});
-    t.tile("j", {32});
-    //t.show();
-    t.interchange("i_p", "j_p");
-    t.show();
-
-  } catch (Error::Error e) {
-    std::cout << e.message_ << "\n";
-  }
-  return 0;
-}
-*/
 #include <QApplication>
 #include "islutils/mainwindow.h"
 #include "islutils/ctx.h"
