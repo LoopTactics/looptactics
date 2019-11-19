261c568271727aaf860a2c4df99ae19602ca778e
lorenzo chelini <l.chelini@icloud.com>
2019-11-19 12:07:30 +0000

    remove deps

--- main.cc
+++ main.cc
@@ -1,15 +1,6 @@
-#include <QApplication>
-#include "islutils/mainwindow.h"
-#include "islutils/ctx.h"
+#include <iostream>
 
-int main(int argc, char **argv) {
-
-  using namespace util;
-  auto ctx = ScopedCtx(pet::allocCtx());
-
-  QApplication app(argc, argv);
-  MainWindow window(ctx);
-  window.showMaximized();
-
-  return app.exec();
+int main() {
+  std::cout << "hello loop tactics" << std::endl;
+  return 0;
 }
